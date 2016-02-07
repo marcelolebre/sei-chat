3ea542913e6720ace7df0a3f5e758c30ae39533b
Marcelo Lebre <marcelo.lebre@gmail.com>
2016-02-07 22:22:25 +0100

    fixing code styling

--- api/policies/isLoggedIn.js
+++ api/policies/isLoggedIn.js
@@ -7,7 +7,7 @@ module.exports = function isLoggedIn(req, res, next) {
       return res.forbidden();
     }else{
     // Redirecting to /login view for browser requests
-       return res.redirect("/login");
+       return res.redirect('/login');
     }
   }
 
